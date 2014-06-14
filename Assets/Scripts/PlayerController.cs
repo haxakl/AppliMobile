6f828db39aa256ef46434291497ef4ca04822424
Guillaume Golfieri <golfieri.guillaume@gmail.com>
2014-06-14 13:53:47 +0200

    Importer le projet unity #1.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerController : MonoBehaviour
+{
+
+		// Player Handling
+		public float gravity = 20;
+		public float speed = 8;
+		public float acceleration = 12;
+		private PlayerPhysics playerPhysics;
+		private float currentSpeed;
+		private float targetSpeed;
+		private Vector2 amountToMove;
+
+		// Use this for initialization
+		void Start ()
+		{
+				playerPhysics = GetComponent<PlayerPhysics> ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				targetSpeed = Input.GetAxisRaw ("Horizontal") * speed;
+				currentSpeed = this.incrementTowards (currentSpeed, targetSpeed, acceleration);
+
+				amountToMove.x = currentSpeed;
+				amountToMove.y -= gravity * Time.deltaTime;
+				playerPhysics.Move (amountToMove * Time.deltaTime);
+		}
+
+		// Increase n towards target by speed
+		private float incrementTowards (float n, float target, float speed)
+		{
+				if (n == target) {
+						return n;
+				} else {
+						float dir = Mathf.Sign (target - n);
+						n += speed * Time.deltaTime * dir;
+						return (dir == Mathf.Sign (target - n)) ? n : target;
+				}
+		}
+}
---
+++ Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class PlayerPhysics : MonoBehaviour {
+
+	public LayerMask collisionMask;
+
+	private BoxCollider collider;
+	private Vector3 size;
+	private Vector3 center;
+
+	private float skin = 0.005f;
+
+	[HideInInspector]
+	public bool grounded;
+
+	Ray ray;
+	RaycastHit hit;
+
+	void Start() {
+		collider = GetComponent<BoxCollider>();
+		size = collider.size;
+		center = collider.center;
+	}
+
+	public void Move(Vector2 amount) {
+		float deltaY = amount.y;
+		float deltaX = amount.x;
+		Vector2 p = transform.position;
+
+		for(int i = 0; i < 3; i++) {
+			float dir = Mathf.Sign (deltaY);
+			float x = (p.x + center.x - size.x/2) + size.x / 2 * i;
+			float y = p.y + center.y + size.y/2 * dir;
+
+			ray = new Ray(new Vector2(x,y), new Vector2(0, dir));
+			Debug.DrawRay(ray.origin, ray.direction);
+
+			if(Physics.Raycast(ray, out hit, Mathf.Abs (deltaY), collisionMask)) {
+				float dst = Vector3.Distance(ray.origin, hit.point);
+
+				if(dst > skin) {
+					deltaY = -dst + skin;
+				} else {
+					deltaY = 0;
+				}
+				grounded = true;
+				break;
+			}
+		}
+
+		Vector2 finalTransform = new Vector2(deltaX, deltaY);
+		transform.Translate(finalTransform);
+	}
+
+}
